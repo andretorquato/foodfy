cba2e4ed18a96c7e9a9cb2bbd7e7bffbe5e0c9dd
AndreTorquato <dede100cmts123@gmail.com>
2020-07-01 12:36:26 -0300

    Button Show/Hide

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,23 @@
 const contents_recipes = document.querySelectorAll('.content_recipe')
+const rows = document.querySelectorAll('.row')
 
 for(let content_recipe of contents_recipes){
     content_recipe.addEventListener('click', function(){
         let idPage = content_recipe.getAttribute('id')
         window.location = `/recipes/${idPage}`
-        
+
+    })
+}
+
+for(let row of rows){
+    const button = row.querySelector('.button')
+    button.addEventListener('click', function(){
+        if(button.innerHTML == "ESCONDER"){
+            row.querySelector('.display').classList.add('hide')
+            button.innerHTML = 'MOSTRAR'
+        }else if(button.innerHTML == "MOSTRAR"){
+            row.querySelector('.display').classList.remove('hide')
+            button.innerHTML = 'ESCONDER'
+        }  
     })
 }
